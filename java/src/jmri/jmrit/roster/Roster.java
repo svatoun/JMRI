03445090772988d0c43712e43680bcadc6e08235
Svata Dedic <svatopluk.dedic@gmail.com>
2020-04-20 00:47:18 +0200

    Allow to replace entire entries.

--- java/src/jmri/jmrit/roster/Roster.java
+++ java/src/jmri/jmrit/roster/Roster.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -111,6 +112,12 @@ public class Roster extends XmlFile implements RosterGroupSelector, PropertyChan
      * entry. {@value #CHANGE}
      */
     public static final String CHANGE = "change"; // NOI18N
+    
+    /**
+     * The entry was replaced.
+     */
+    public static final String REPLACE = "replace"; // NOI18N
+    
     /**
      * Property change event fired when saving the roster. {@value #SAVED}
      */
@@ -227,6 +234,24 @@ public class Roster extends XmlFile implements RosterGroupSelector, PropertyChan
         this.addRosterGroups(e.getGroups(this));
         setDirty(true);
         firePropertyChange(ADD, null, e);
+    }
+    
+    public boolean replaceEntry(RosterEntry old, RosterEntry e) {
+        if (!Objects.equals(old.getId(), e.getId())) {
+            throw new IllegalArgumentException();
+        }
+        synchronized (this) {
+            int i = _list.indexOf(old);
+            if (i == -1) {
+                return false;
+            }
+            _list.set(i, e);
+            e.addPropertyChangeListener(this);
+        }
+        this.addRosterGroups(e.getGroups(this));
+        setDirty(true);
+        firePropertyChange(REPLACE, null, e);
+        return true;
     }
 
     /**
@@ -830,7 +855,7 @@ public class Roster extends XmlFile implements RosterGroupSelector, PropertyChan
             log.debug("no roster file found; this is normal if you haven't put decoders in your roster yet");
             return;
         }
-
+        
         // find root
         Element root = rootFromName(name);
         if (root == null) {
